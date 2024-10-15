0a2885f91618c883dc9b2c7e1b82382870317656
Sameer Haq <sameerhaq@Sameers-MacBook-Pro.local>
2024-10-15 12:30:30 -0500

    hi

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
           <Image
             src="/hero-desktop.png"
             width={1000}
-            height={760}
+            height={761}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
